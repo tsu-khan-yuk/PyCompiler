bddfc1540dd9d486f72ffc6d62d1e58e2819c1b2
tsu-khan-yuk <62913085+tsu-khan-yuk@users.noreply.github.com>
2020-05-02 00:36:58 +0300

    modification 3

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
 ###########################################################################################################
 # -> Выполнил: Иван Суханюк
 # -> Дата создания: 16.04.2020
-# -> Последние изменения: 16.04.2020
+# -> Последние изменения: 02.05.2020
 # -> Связанеые файлы: assembly.txt
 ###########################################################################################################
 assembly = open("assembly.txt", "rt")
 listing = open("main.txt", "w")
 count = 0
-for i in assembly:
+for line in assembly:
     assume = False
     count += 1
-    listing.write("Рядок номер [" + str(count) + "]:\t" + i)
-    lst1 = i.split()
-    for j in lst1:
-        if j == "MACRO":
-            listing.write(">>> Ідентифікатор директиви макровизначення\n\n")
-        elif j == "SEGMENT":
-            listing.write(">>> Ідентифікатор директиви початку сегменту\n\n")
-        elif j == "DB":
-            listing.write(">>> Ідентрифікатор директиви 1 байтового типу данних\n\n")
-        elif j == "DW":
-            listing.write(">>> Ідентрифікатор директиви 2-х байтового типу данних\n\n")
-        elif j == "DD":
-            listing.write(">>> Ідентрифікатор директиви 4-х байтового типу данних\n\n")
-        elif j == "ENDS":
-            listing.write(">>> Ідентрифікатор директиви закінчення сегменту\n\n")
-        elif j == "ENDM":
-            listing.write(">>> Ідентрифікатор директиви закінчення макровизначення\n\n")
-        elif j == "END":
-            listing.write(">>> Ідентифікатор директиви закінчення програми\n\n")
-        elif j == "AX":
-            listing.write(">>> Ідентифікатор регістора аккумулятора")
-        elif j == "BX":
-            listing.write(">>> Ідентифікатор ")
-        elif j == "DI":
-            listing.write(">>> ")
-        elif j == "SI":
-            listing.write(">>> ")
-        elif j == "BP":
-            listing.write(">>> ")
-        elif j == "Inc" or j == "Mov" or j == "Or" or j == "Cmp" or j == "Adc" or j == "Cbw" or \
-                j == "And" or j == "Jbe":
-            listing.write(">>> Ідентифікатор мнемонічного коду машинної інструкції\n\n")
-        elif j.startswith("CS") or j.startswith("ES") or j.startswith("DS"):
+    listing.write(f"Рядок номер [{str(count)}]:\t{line}")
+    lst1 = line.split()
+    for word in lst1:
+        listing.write(f"\t{word} ")
+        if word == "MACRO":
+            listing.write(">>> Ідентифікатор директиви макровизначення\n")
+        elif word == "SEGMENT":
+            listing.write(">>> Ідентифікатор директиви початку сегменту\n")
+        elif word == "DB":
+            listing.write(">>> Ідентрифікатор директиви 1 байтового типу данних\n")
+        elif word == "DW":
+            listing.write(">>> Ідентрифікатор директиви 2-х байтового типу данних\n")
+        elif word == "DD":
+            listing.write(">>> Ідентрифікатор директиви 4-х байтового типу данних\n")
+        elif word == "ENDS":
+            listing.write(">>> Ідентрифікатор директиви закінчення сегменту\n")
+        elif word == "ENDM":
+            listing.write(">>> Ідентрифікатор директиви закінчення макровизначення\n")
+        elif word == "END":
+            listing.write(">>> Ідентифікатор директиви закінчення програми\n")
+        elif word == "AX":
+            listing.write(">>> Ідентифікатор регістра аккумулятора\n")
+        elif word == "BX":
+            listing.write(">>> Ідентифікатор регістра бази\n")
+        elif word == "DI":
+            listing.write(">>> Ідертифікатор регітсра DI\n")
+        elif word == "SI":
+            listing.write(">>> Ідентифікатор регістра SI\n")
+        elif word == "BP":
+            listing.write(">>> Ідентифікатор регітсра BP\n")
+        elif word == "Inc" or word == "Mov" or word == "Or" or word == "Cmp" or word == "Adc" or word == "Cbw" or \
+                word == "And" or word == "Jbe":
+            listing.write(">>> Ідентифікатор мнемонічного коду машинної інструкції\n")
+        elif word.startswith("CS") or word.startswith("ES") or word.startswith("DS"):
             listing.write(">>> Ідетифікатор сегментного регістра")
-            lst2 = j.split(":")
+            lst2 = word.split(":")
             for k in lst2:
                 if k == "CS":
                     listing.write(" кодів\n")
@@ -54,6 +55,7 @@ for i in assembly:
                     listing.write(" данних\n")
         else:
             listing.write(">>> Ідентифікатор невизнаечений, або мітка\n")
+    listing.write("\n")
 
 
 assembly.close()

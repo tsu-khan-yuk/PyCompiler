a429118ca047f7319f9ebbbeb63facb10973d530
tsu-khan-yuk <vania8282125@gmail.com>
2020-05-30 01:49:46 +0300

    mod 1

--- asm_types.py
+++ asm_types.py
@@ -12,32 +12,38 @@
 
 
 class Opcode:
-    _first_operand = None
-    _second_operand = None
-    _value = None
-    _index = None
+    __first_operand = None
+    __second_operand = None
+    __mod = None
+    __value = None
+    __index = None
 
-    def __init__(self, f=None, s=None, val=None):
-        self._first_operand = f
-        self._second_operand = s
-        self._value = val
+    def __init__(self, mod=False, f=None, s=None, val=None):
+        self.__mod = mod
+        self.__first_operand = f
+        self.__second_operand = s
+        self.__value = val
 
     def __getitem__(self, item):
-        if 0 <= item <= len(self._value):
-            return [self._first_operand[item], self._second_operand[item], self._value[item]]
+        if 0 <= item <= len(self.__value):
+            return [self.__first_operand[item], self.__second_operand[item], self.__value[item]]
 
 
 comand = {
-    "Cbw": Opcode(None, None, "0x98"),
-    "Inc": Opcode(None, None, "0xff"),
-    "Adc": Opcode(["r/m8", "r/m16", "r8", "r16"], ["r8", "r16", "r/m8", "r/m16"], ["0x10", "0x11", "0x12", "0x13"]),
-    "Cmp": Opcode(["r8", "r16"], ["r16", "r/m16"], ["0x3a", "0x3b"]),
-    "And": Opcode(["r/m8", "r/m16"], ["r8", "r16"], ["0x20", "0x21"]),
-    "Mov": Opcode(["r8", "r16", "r/m8", "r/m16"], ["imm8", "imm16", "imm8", "imm16"],
+    #todo: изменить структуру с учетом mod r/m
+    "Cbw": Opcode(False, None, None, "0x98"),
+    "Inc": Opcode(False, None, None, "0xff"),
+    "Adc": Opcode(False, ["r/m8", "r/m16", "r8", "r16"], ["r8", "r16", "r/m8", "r/m16"],
+                  ["0x10", "0x11", "0x12", "0x13"]),
+    "Cmp": Opcode(False, ["r8", "r16"], ["r16", "r/m16"], ["0x3a", "0x3b"]),
+    "And": Opcode(False, ["r/m8", "r/m16"], ["r8", "r16"], ["0x20", "0x21"]),
+    "Mov": Opcode(False, ["r8", "r16", "r/m8", "r/m16"], ["imm8", "imm16", "imm8", "imm16"],
                   ["0xb0", "0xb8", "0xc6", "0xc7"]),
-    "Or": Opcode(["r/m8", "r/m16", "r/m16"], ["imm8", "imm16", "imm8"], ["0x80", "0x81", "0x83"]),
-    "Jbe": Opcode(["rel8", "rel16/32"], None, ["0x76", "0x86"])
+    "Or": Opcode(False, ["r/m8", "r/m16", "r/m16"], ["imm8", "imm16", "imm8"], ["0x80", "0x81", "0x83"]),
+    "Jbe": Opcode(False, ["rel8", "rel16/32"], None, ["0x76", "0x86"])
 }
+
+# todo: добавить структуру для записи комнад и строчек сегментов
 
 segment_tab = {
     "CS": None,
